9c34ea3f4b28dfc304db6aace72d935120b56894
Hasan Bakirtas <serdarbakirtas@gmail.com>
2019-08-12 23:57:01 +0300

    Add constant value as less than or equal to in NSLayout extentions

--- Extensions/NSLayoutExtensions.swift
+++ Extensions/NSLayoutExtensions.swift
@@ -20,4 +20,24 @@ public extension NSLayoutDimension {
         cons.isActive = true
         return cons
     }
+    
+    /// Sets dimension to a constant value as less than or equal to
+    ///
+    /// - Parameters:
+    ///   - constant: Value to set
+    ///   - priority: Priority of the constraint (defaults to required)
+    /// - Returns: Already activated NSLayoutConstraint instance
+    /// - Example: view.widthAnchor.cuiSet(lessThanOrEqualTo: CGFloat(50.0))
+    @discardableResult
+    func cuiSet(
+        lessThanOrEqualTo constant: CGFloat,
+        priority: UILayoutPriority = .required
+        ) -> NSLayoutConstraint {
+        
+        let cons = constraint(lessThanOrEqualToConstant: constant)
+        cons.priority = priority
+        cons.isActive = true
+        return cons
+    }
+    
 }
