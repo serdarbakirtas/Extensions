3add47ae0d56f73e418781fb24fc57c2d56dc2c1
Hasan Bakirtas <serdarbakirtas@gmail.com>
2019-08-12 23:52:49 +0300

    Add navigation controller

---
+++ Extensions/UINavigationViewExtensions.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+// MARK: - Hierarchy
+
+public extension UINavigationController {
+    
+    func viewControllers<T>() -> [T] {
+        
+        return viewControllers.compactMap {
+            $0 as? T
+        }
+    }
+    
+    func firstViewController<T>() -> T? {
+        
+        return viewControllers().first
+    }
+    
+    func lastViewController<T>() -> T? {
+        
+        return viewControllers().last
+    }
+}
+
+// MARK: - Push/Pop with completion blocks
+
+public extension UINavigationController {
+    
+    func pushViewController(
+        viewController: UIViewController,
+        animated: Bool,
+        completion: (() -> Void)? = nil
+        ) {
+        
+        pushViewController(viewController, animated: animated)
+        
+        if let coordinator = transitionCoordinator, animated {
+            coordinator.animate(alongsideTransition: nil) { _ in completion?() }
+        } else {
+            DispatchQueue.main.async { completion?() }
+        }
+    }
+    
+    
+}
