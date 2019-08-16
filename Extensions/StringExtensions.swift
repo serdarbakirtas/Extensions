0d6e6184f5b9df2924dd61dc5b3904e17a092e5f
Hasan Bakirtas <serdarbakirtas@gmail.com>
2019-08-17 00:04:19 +0300

    Add email validation in string extentions

--- Extensions/StringExtensions.swift
+++ Extensions/StringExtensions.swift
@@ -48,4 +48,11 @@ extension String {
         let filtredUnicodeScalars = unicodeScalars.filter{CharacterSet.decimalDigits.contains($0)}
         return String(String.UnicodeScalarView(filtredUnicodeScalars))
     }
+
+    func isValidEmail(testStr: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
 }
