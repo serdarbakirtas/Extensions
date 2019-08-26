eedefd105b5e14d1a07c1b9fe100c5b9872b9914
Hasan Bakirtas <serdarbakirtas@gmail.com>
2019-08-27 00:12:33 +0300

    Add date extensions

---
+++ Extensions/DateExtensions.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+extension Date {
+    
+    func daysBetween(date: Date) -> Int {
+        return Date.daysBetween(start: self, end: date)
+    }
+    
+    static func daysBetween(start: Date, end: Date) -> Int {
+        let calendar = Calendar.current
+        
+        
+        let date1 = calendar.startOfDay(for: start)
+        let date2 = calendar.startOfDay(for: end)
+        
+        let a = calendar.dateComponents([.day], from: date1, to: date2)
+        return a.value(for: .day)!
+    }
+    
+    func currentDate() -> Date {
+        
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let formattedDate = dateFormatter.string(from: date)
+        
+        let now = dateFormatter.date(from: formattedDate)
+        guard let nowDate = now else { return Date() }
+        return nowDate
+    }
+}
